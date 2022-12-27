8648007a762fce239217bcc11210144e678778e7
Karina Davydenko <92226081+wpk02@users.noreply.github.com>
2022-12-27 19:47:35 +0300

    add json diff

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,63 @@
 import { program } from "commander";
+import path from "path";
+import fs from "fs";
+import _ from "lodash";
 
 program
+  .arguments("<filename1> <filename2>")
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
   .option("-h, --help", "display help for command")
   .option("-f, --format <type>", "output format")
-  .version("0.0.1");
+  .version("0.0.1")
+  .action((filename1, filename2) => {
+    if (!filename1.endsWith(".json") || !filename2.endsWith(".json")) {
+      throw new Error("Files should be in JSON format!");
+    }
+
+    const fullName1 = path.resolve(filename1);
+    const fullName2 = path.resolve(filename2);
+
+    const object1 = JSON.parse(fs.readFileSync(fullName1));
+    const object2 = JSON.parse(fs.readFileSync(fullName2));
+
+    const map = new Map();
+
+    for (const [key, value] of Object.entries(object1)) {
+      if (!Object.hasOwn(object2, key)) {
+        map.set(`  - ${key}`, value);
+      } else {
+        const value2 = object2[key];
+
+        if (value === value2) {
+          map.set(`    ${key}`, value);
+        } else {
+          map.set(`  - ${key}`, value);
+          map.set(`  + ${key}`, value2);
+        }
+      }
+    }
+
+    for (const [key, value] of Object.entries(object2)) {
+      if (!Object.hasOwn(object1, key)) {
+        map.set(`  + ${key}`, value);
+      }
+    }
+
+    const sortedProps = _.sortBy(Array.from(map), [
+      ([key]) => {
+        return _.trimStart(key, " +-");
+      },
+    ]);
+
+    console.log("{");
+
+    sortedProps.forEach(([key, value]) => {
+      console.log(`${key}: ${value}`);
+    });
+
+    console.log("}");
+  });
 
 program.parse();
 
